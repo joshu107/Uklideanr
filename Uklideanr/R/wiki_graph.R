78477997cab94e427e38260c8f559e54a06423b6
unknown <joshu107@PC5-1.helix.ida.liu.se>
2016-09-22 22:08:20 +0200

    Added data set "wiki_graph" and documentation to package

---
+++ Uklideanr/R/wiki_graph.R
@@ -0,0 +1,13 @@
+#' Graph data from Wikipedia
+#' 
+#' 
+#' A data set containing the edges and edge weights of the first graph on Dijkstra's algorithm's Wikipedia page  
+#' 
+#' @format A data frame with 18 rows and 3 variables:
+#' \describe{
+#'    \item{v1}{first edge of the graph}
+#'    \item{v2}{second edge of the graph}
+#'    \item{w}{weight of the edge}
+#' }
+#' @references https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm
+"wiki_graph"
